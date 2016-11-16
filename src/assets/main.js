fde7edba1085e324ba3a132973af3f7bc0d4aacc
NicoleBencomoCole <nicolebencomo@gmail.com>
2016-11-15 17:22:45 -0700

    My first commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
+	$.ajax({
+		url:"https://www.codeschool.com/users/NicoleCole.json",
+		dataType:"jsonp",
+		success:function(response){
+			
+		
+		}
 
-  // your code will go here
-
+	});
 });
